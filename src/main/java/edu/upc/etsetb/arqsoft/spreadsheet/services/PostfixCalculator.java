dd5a58291910728e8399ac3284c8d531ee80f18e
Marta <martacosanoserra@gmail.com>
2021-06-26 00:03:26 +0200

    metode load from file fet

--- src/main/java/edu/upc/etsetb/arqsoft/spreadsheet/services/PostfixCalculator.java
+++ src/main/java/edu/upc/etsetb/arqsoft/spreadsheet/services/PostfixCalculator.java
@@ -2,11 +2,8 @@
 package edu.upc.etsetb.arqsoft.spreadsheet.services;
 
 import edu.upc.etsetb.arqsoft.spreadsheet.enties.Component;
-import edu.upc.etsetb.arqsoft.spreadsheet.enties.Operand;
 import edu.upc.etsetb.arqsoft.spreadsheet.entities.impl.MyNumber;
-import edu.upc.etsetb.arqsoft.spreadsheet.entities.impl.Operator;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /*
@@ -27,20 +24,16 @@ public class PostfixCalculator {
     
     
     public MyNumber postfixEvaluation(List<Component> postfixExpression){
-        //inicialitza el stack perq sem crea buida
-    //    LinkedList<Component> stack = new LinkedList<>();
+     
         List<Component> stack = new ArrayList<>();
         
         int i=0;
-        System.out.println("postifexpression");
-        System.out.println(postfixExpression.size());
+       
         while( i < postfixExpression.size()){
             Component elem= postfixExpression.get(i);
             
             if(this.isOperator(elem)){ //operator
              
-               // Component topNumber =stack.pop();
-               // Component bottomNumber =stack.pop();
                 Component topNumber =stack.remove(stack.size()-1);
                 Component bottomNumber =stack.remove(stack.size()-1);
                 Component result= computeResult(bottomNumber, elem, topNumber );
@@ -64,15 +57,9 @@ public class PostfixCalculator {
     private Boolean isOperator(Component component){
         
         String comp_string = component.getValueAsString();
-        String operatorsAvailable ="+-*/";
-        operatorsAvailable.contains(comp_string);
-     
-        if(operatorsAvailable.contains(comp_string)){
-            return true;
-        }else{
-            return false;
-        }
-        
+        String operatorsAvailable ="+-*/"; 
+        return(operatorsAvailable.contains(comp_string));
+          
         
     }
     
